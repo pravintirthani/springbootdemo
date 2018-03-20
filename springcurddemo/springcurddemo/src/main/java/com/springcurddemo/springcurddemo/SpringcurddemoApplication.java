e8fcbade81a15177a2e5dbe97b17d4ec833ac5f6
PRavinTirthani <pravintirthani@yahoo.com>
2018-03-20 08:04:15 +0530

    Intially push

---
+++ springcurddemo/springcurddemo/src/main/java/com/springcurddemo/springcurddemo/SpringcurddemoApplication.java
@@ -0,0 +1,12 @@
+package com.springcurddemo.springcurddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcurddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcurddemoApplication.class, args);
+	}
+}
